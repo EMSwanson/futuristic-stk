c581259dc6ae30863e4ba4af9fcb1f48e13eafdc
Eli Swanson <eliswanson@gmail.com>
2020-08-25 21:39:31 -0500

    removing gather

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
-from gatherer import gather
+"""Load data from apis.
+"""
 
-external_data = gather()
+import pandas as pd
+from quandl_unemployment.api import API as QAPI
+from dbnomics.api import API as DAPI
+from dbnomics.config import urls as dbnomics_urls
+
+
+def gather(filename="api_data.csv") -> pd.DataFrame():
+    quandl_data = QAPI()
+    quandl_data.get_data()
+    quandl_data.add_colnames()
+    quandl_df = quandl_data.df
+    quandl_df.to_csv(filename, encoding='utf-8')
+    return quandl_df
